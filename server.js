6d744a4b814a46d9b176a0290cfb15648116e474
Aubrey Jones <aubrey_jones07@hotmail.com>
2025-09-01 13:39:39 -0600

    started adding in mongodb database

--- server.js
+++ server.js
@@ -3,17 +3,32 @@ import express from 'express';
 //import helmet from 'helmet'; 
 //import compression from 'compression'; 
 import * as dotenv from 'dotenv'; 
-//import cors from 'cors'; 
+import cors from 'cors'; 
 
+
+//hidden files
 
 dotenv.config()
+ 
 
-
-const port = process.env.PORT; 
+//adding express
 
 const server = express()
 
-//server.use(cors())
+//CORS Issues Resolution: 
+
+server.use(cors())
+
+server.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "*")
+})
 
 server.use(express.urlencoded({extended: true}))
 
@@ -25,12 +40,31 @@ server.use(express.json())
 
 server.use(express.static('public'))
 
+//homesAPIRouter
+
+server.use('/homesforsale', homesForSaleRouter)
 
 
+//if Page Not Found: 
 
-server.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+server.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
+
+
+// MongoDB :) in a try/catch promise
+
+
+const port = process.env.PORT;
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(process.env.DB_CONN_STRING, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}).then(() => {
+    server.listen(PORT, () => console.log(`Server Started: ${PORT}`));
+}).catch((error) => console.log(`${error} sorry not sorry. That didn't work. Try again.`)); 
 
 
 
